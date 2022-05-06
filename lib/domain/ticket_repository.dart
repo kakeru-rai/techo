255525f09b2300f9e71469d87fac6fb0e34e52ea
頼　翔 <raix@raishounoMacBook-Air.local>
2022-05-06 15:46:48 +0900

    lint warn unused_importを解消

--- lib/domain/ticket_repository.dart
+++ lib/domain/ticket_repository.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_hello_world/domain/ticket.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
