8fb7902800f57a1fdd9783dd6a4474b408595350
Kakeru Rai <raix5867@gmail.com>
2022-05-07 18:27:30 +0900

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ import 'my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'gen/firebase_options_dev.dart';
 
-import 'shared/logger.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
