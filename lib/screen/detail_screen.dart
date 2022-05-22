57b0f2bbc30014d0bd9b825dd0ea4d2a3d2edfe8
Kakeru Rai <raix5867@gmail.com>
2022-05-22 12:10:58 +0900

    ボタンを配置

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -55,11 +55,24 @@ class _DetailScreenState extends State<DetailScreen> {
     super.dispose();
   }
 
-  void _setPreview(bool isPreview) {
+  void _setStatePreview(bool isPreview) {
     setState(() {
       _isPreview = isPreview;
       _uiBuilder =
           _isPreview ? _PreviewModeScreen(this) : _EditModeScreen(this);
+    });
+  }
+
+  void _setStateMdTag(String tag) {
+    setState(() {
+      var newCusorPosition = currentLineHeadPosition(
+          _bodyController.text, _bodyController.selection.start);
+      var text =
+          addMdTag(_bodyController.text, tag, _bodyController.selection.start);
+      _bodyController.text = text;
+
+      _bodyController.selection =
+          TextSelection.fromPosition(TextPosition(offset: newCusorPosition));
     });
   }
 
@@ -71,45 +84,34 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        // ユーザー操作による「戻る」操作
-        onWillPop: () {
-          if (Navigator.of(context).userGestureInProgress) {
-            // iosの戻るジェスチャー
-            _save();
-            return Future.value(true);
-          }
-          // iosの戻るジェスチャー意外 == Androidのバックキー
-          if (_isPreview) {
-            _save();
-            return Future.value(true);
-          } else {
-            _setPreview(true);
-            return Future.value(false);
-          }
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(widget.ticket.title),
-            leading: Builder(
-              builder: (BuildContext context) {
-                return _uiBuilder.appBarLeadingIconButton();
-              },
-            ),
+      // ユーザー操作による「戻る」操作
+      onWillPop: () {
+        if (Navigator.of(context).userGestureInProgress) {
+          // iosの戻るジェスチャー
+          _save();
+          return Future.value(true);
+        }
+        // iosの戻るジェスチャー意外 == Androidのバックキー
+        if (_isPreview) {
+          _save();
+          return Future.value(true);
+        } else {
+          _setStatePreview(true);
+          return Future.value(false);
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.ticket.title),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return _uiBuilder.appBarLeadingIconButton();
+            },
           ),
-          body: _uiBuilder.scaffoldBody(),
-          floatingActionButton: FloatingActionButton(onPressed: () {
-            setState(() {
-              var newCusorPosition = currentLineHeadPosition(
-                  _bodyController.text, _bodyController.selection.start);
-              var text = addMdTag(
-                  _bodyController.text, "#", _bodyController.selection.start);
-              _bodyController.text = text;
-
-              _bodyController.selection = TextSelection.fromPosition(
-                  TextPosition(offset: newCusorPosition));
-            });
-          }),
-        ));
+        ),
+        body: _uiBuilder.scaffoldBody(),
+      ),
+    );
   }
 }
 
@@ -141,7 +143,7 @@ String addMdTag(String text, String tag, int cursorPosition) {
   var textToCursorPosition = text.substring(0, cursorPosition);
   var cursorLineCount = textToCursorPosition.split("\n").length - 1;
   String lineText = text.split("\n")[cursorLineCount];
-  var match = RegExp(r"" + tag + "+? ").firstMatch(lineText);
+  var match = RegExp(r"\" + tag + "+? ").firstMatch(lineText);
   var addTagText = match == null ? tag + " " : tag;
 
   return text.substring(0, addPosition) +
@@ -163,41 +165,72 @@ class _EditModeScreen extends DetailScreenUiBuilder {
     return IconButton(
       icon: const Icon(Icons.check),
       onPressed: () {
-        parent._setPreview(true);
+        parent._setStatePreview(true);
       },
     );
   }
 
   @override
   Widget scaffoldBody() {
-    return SingleChildScrollView(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            controller: parent._titleController,
-            decoration: const InputDecoration(
-              hintText: 'タイトル',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            controller: parent._bodyController,
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            onChanged: parent._onBodyChanged,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              hintText: "本文",
-            ),
-          ),
-        ),
-      ],
-    ));
+    return Container(
+        color: Colors.grey[100],
+        child: Column(children: [
+          Expanded(
+              child: SingleChildScrollView(
+                  child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: Container(
+                    color: Colors.white,
+                    child: TextField(
+                      controller: parent._titleController,
+                      decoration: const InputDecoration(
+                        hintText: 'タイトル',
+                      ),
+                    ),
+                  )),
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: Container(
+                      color: Colors.white,
+                      child: TextFormField(
+                        controller: parent._bodyController,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        onChanged: parent._onBodyChanged,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          hintText: "本文",
+                        ),
+                      ))),
+            ],
+          ))),
+          Container(
+              decoration: const BoxDecoration(
+                color: Colors.white70,
+              ),
+              child: Row(children: [
+                OutlinedButton(
+                    child: Text("見出し"),
+                    onPressed: (() {
+                      parent._setStateMdTag("#");
+                    })),
+                OutlinedButton(
+                    child: Text("・"),
+                    onPressed: (() {
+                      parent._setStateMdTag("-");
+                    })),
+                OutlinedButton(
+                    child: Text("1."),
+                    onPressed: (() {
+                      parent._setStateMdTag("1.");
+                    })),
+              ]))
+        ]));
   }
 }
 
@@ -221,7 +254,7 @@ class _PreviewModeScreen extends DetailScreenUiBuilder {
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
         child: GestureDetector(
             onTap: () {
-              parent._setPreview(false);
+              parent._setStatePreview(false);
             },
             child: parent.markdown.isEmpty
                 ? const Text("まだ何も入力されていません。タップして入力を開始。",
@@ -232,7 +265,7 @@ class _PreviewModeScreen extends DetailScreenUiBuilder {
                     shrinkWrap: true,
                     softLineBreak: true,
                     onTapText: () {
-                      parent._setPreview(false);
+                      parent._setStatePreview(false);
                     },
                   )));
   }
